6c28d7be86454efacde57d3ec44e7682953ea2a8
Reina234 <zhengreina@gmail.com>
2024-12-28 21:07:37 +0800

    deleted a whole bunch of stuff, no clue if it broke everything

--- parser_test.py
+++
@@ -1,4 +0,0 @@
-from ZZ_parsers.file_splitter import FileSplitter
-
-file_splitter = FileSplitter()
-print(file_splitter.split_file("hexane.itp"))
--- test_27_dec_2.py
+++
@@ -1,5 +0,0 @@
-from A_modules.atomistic.gromacs.parser.gromacs_parser import GromacsParser
-
-parser = GromacsParser()
-sections = parser.parse("TRIAL/acpype_output/POLY_GMX.top")
-print(sections)
