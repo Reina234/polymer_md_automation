3dcbd97e10c6c58ee0825e86e268a48fd5c640b5
Reina234 <zhengreina@gmail.com>
2025-01-27 22:51:39 +0000

    commit 1 out of large batch from refactor

--- generator_test.py
+++
@@ -1,5 +0,0 @@
-from modules.workflows.atomistic.polymer_parametizer import PolymerGeneratorWorkflow
-
-generator = PolymerGeneratorWorkflow(["C=Cc1ccccc1", "C=C"], 10, output_dir="test")
-
-generator.run()
--- open_mscg_map_test.py
+++
@@ -1,8 +0,0 @@
-from mscg import *
-from mscg.cli import cgmap
-
-cgmap.main(map="zzz/test_open_msg.yaml", traj="temp/production.trr", out="CG.lammpstrj")
-
-CG_traj = Trajectory("CG.lammpstrj", fmt="lammpstrj")
-CG_traj.read_frame()
-CG_traj.x.shape
